77ac079d30613a773880aecf143bcafd58c88ad8
Dutch Adachi <dutch@adachis.info>
2019-04-02 00:12:25 -0400

    Temp converter mark II

--- src/app.vue
+++ src/app.vue
@@ -1,15 +1,39 @@
 <template>
-<div class="main">{{message}}</div>
+    <div class="main">
+        <div class="degreeC">
+            <label id="labelForDegreeC">Degree C</label>
+        <input id="degreeC" v-model.lazy="degreeC"/>
+        </div>
+        <div class="ferhC">
+            <label id="labelForDegreeF">Degree F</label>
+            <input id="degreeF" v-model.lazy="degreeF">
+        </div>
+
+
+    </div>
 </template>
 
 <script lang="ts">
+    /**
+     * Use of getter and setter to computed value to simplify two way conversion of temperature.
+     */
     import Vue from 'vue';
+    import * as numeral from 'numeral'
     export default Vue.extend(
         {
             name: 'MainApp',
-            data(){
+            data() {
                 return {
-                    message: 'Hello World,  told from the point of vue!'
+                    degreeC: '0'
+                }
+            },
+            computed: {
+                degreeF: {
+                    get(): string   { return numeral((parseFloat(this.degreeC) *9/5) +32).format('0.0') }
+                    ,
+                    set(newValue:string ): void  {
+                        this.degreeC = numeral((parseFloat(newValue) - 32) * 5 / 9).format('0.0')
+                    }
                 }
             }
         }
@@ -17,7 +41,7 @@
 </script>
 
 <style scoped>
- .main {
-     color:green;
- }
+    .main {
+        color: green;
+    }
 </style>
