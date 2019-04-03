583230a72e6682f204bbb5038c71a8aa07dc01ec
Dutch Adachi <dutch@adachis.info>
2019-04-02 22:57:16 -0400

    Temp converter mark II take 2

--- src/app.vue
+++ src/app.vue
@@ -2,11 +2,11 @@
     <div class="main">
         <div class="degreeC">
             <label id="labelForDegreeC">Degree C</label>
-        <input id="degreeC" v-model.lazy="degreeC"/>
+            <input id="degreeC" v-model.lazy="degreeC" value="0.0"/>
         </div>
         <div class="ferhC">
             <label id="labelForDegreeF">Degree F</label>
-            <input id="degreeF" v-model.lazy="degreeF">
+            <input id="degreeF" v-model.lazy="degreeF" value="32.0">
         </div>
 
 
@@ -19,23 +19,36 @@
      */
     import Vue from 'vue';
     import * as numeral from 'numeral'
+
     export default Vue.extend(
         {
             name: 'MainApp',
             data() {
                 return {
-                    degreeC: '0'
+                  degreeCSeed: '0.0'
                 }
             },
             computed: {
                 degreeF: {
-                    get(): string   { return numeral((parseFloat(this.degreeC) *9/5) +32).format('0.0') }
+                    get(): string {
+                        return numeral((parseFloat(this.degreeC) * 9 / 5) + 32).format('0.0')
+                    }
                     ,
-                    set(newValue:string ): void  {
-                        this.degreeC = numeral((parseFloat(newValue) - 32) * 5 / 9).format('0.0')
+                    set(newValue: string): void {
+                        this.degreeCSeed = numeral((parseFloat(newValue) - 32) * 5 / 9).format('0.0')
+                    }
+
+                },
+                degreeC: {
+                    get(): string {
+                        return numeral(parseFloat(this.degreeCSeed)).format('0.0')
+                    },
+                    set(newValue: string): void {
+                        this.degreeCSeed = numeral(parseFloat(newValue)).format('0.0');
                     }
                 }
             }
+
         }
     )
 </script>
