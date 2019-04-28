72db2f7047ce4630609ac0c0be35864705371767
Dutch Adachi <dutch@adachis.info>
2019-04-28 15:39:38 -0400

    Finished Lesson 2 (but already using SFC) removed @types/vue - not needed.

--- src/app.vue
+++ src/app.vue
@@ -2,20 +2,22 @@
     <div class="container">
         <div class="card mt-5">
             <h2 class="card-header">Near-Earth Objects</h2>
-            <div class="m-3" v-cloak v-if="numAsteroids>0">
+            <div class="m-3">
                 <p>Showing {{numAsteroids}} items</p>
+                <p>{{closestObject.name}} has the shortest missing distance of {{closestObject.miles}} miles </p>
             </div>
 
             <table class="table" v-cloak>
 
                 <thead class="thead-light">
                 <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Close Approach Date</th>
-                <th>Miss Distance</th>
-                <th>Remove</th>
-                </tr>`
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Close Approach Date</th>
+                    <th>Miss Distance</th>
+                    <th>Remove</th>
+                </tr>
+                `
                 </thead>
                 <tbody>
                 <tr v-for="(a,index) in asteroids" :key="a.neo_reference_id">
@@ -29,7 +31,9 @@
                             </li>
                         </ul>
                     </td>
-                    <td><button @click="remove(index)" class="bton btn-warning">remove</button></td>
+                    <td>
+                        <button @click="remove(index)" class="bton btn-warning">remove</button>
+                    </td>
                 </tr>
                 </tbody>
             </table>
@@ -38,9 +42,11 @@
 </template>
 
 <script lang="ts">
-    /**
-     * Use of getter and setter to computed value to simplify two way conversion of temperature.
-     */
+
+    interface simpleNeo {
+        name:string;
+        miles:number;
+    }
     import Vue from 'vue';
     import axios from 'axios';
 
@@ -51,41 +57,59 @@
                     asteroids: [] as any
                 }
             },
+            computed: {
+                numAsteroids():number { //without type annotation, Typescript will complain about missing type of 'this'
+                    return this.asteroids.length;
+                },
+                closestObject():simpleNeo {
+                    let neoWithData = this.asteroids.filter(function (neo: any) {
+                        return neo.close_approach_data.length > 0;
+                    });
+                    let simpleNeos = neoWithData.map(function (neo: any) {
+                        return {
+                            name: neo.name,
+                            miles: neo.close_approach_data[0].miss_distance.miles
+                        }
+                    });
+                    let sortedNeos = simpleNeos.sort(function (a: any, b: any) {
+                        return a.miles - b.miles;
+                    });
+                    return sortedNeos[0]
+                }
+            },
             methods: {
-                fetchAsteroids : function () {
+                fetchAsteroids: function () {
                     let apiKey = 'DEMO_KEY'; //replace with real apikey. demo key is for limited use only
 
                     let url = 'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=' + apiKey;
                     axios.get(url)
                         .then((res) => {
-                            (this as any).asteroids = res.data.near_earth_objects.slice(0, 15);
+                            this.asteroids = res.data.near_earth_objects.slice(0, 15);
                         })
                 },
-                 getCloseApproachDate: function(a:any){
-                    if (a.close_approach_data.length>0){
+                getCloseApproachDate: function (a: any) {
+                    if (a.close_approach_data.length > 0) {
                         return a.close_approach_data[0].close_approach_date
                     }
                     return 'N/A'
 
                 },
-                remove: function(index:number){
-                    (this as any).asteroids.splice(index,1)
+                remove: function (index: number) {
+                    this.asteroids.splice(index, 1)
                 }
             },
 
             created: function () {
                 (this as any).fetchAsteroids();
-            },
-            computed: {
-                numAsteroids: function(){
-                    return ((this as any).asteroids.length as number)
-                }
             }
+
 
         }
     )
 </script>
 
 <style scoped>
-    [v-cloak] {display:none}
+    [v-cloak] {
+        display: none
+    }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './app.vue';
 import './assets/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'promise-polyfill/src/polyfill';
-let vm = new Vue(
+const vm = new Vue(
     {
         el: '#app',
         render: function(h:any){ return h(App)}
