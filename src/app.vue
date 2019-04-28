ca23bef692a0542c2409701a948d85b72b14da6e
Dutch Adachi <dutch@adachis.info>
2019-04-28 17:22:25 -0400

    Fixed APOD to handle the case if url is not a jpg but youtube video.

--- src/app.vue
+++ src/app.vue
@@ -2,15 +2,14 @@
     <div class="container">
 
             <h2 class="card-header">Astronomy Picture of the Day. {{date}}</h2>
-            <img :src="url" class="img-fluid" :title="title">
-            <h3 class="card-footer">from NASA.gov</h3>
+            <img v-if="urlIsImage" :src="url" class="img-fluid" :title="title">
+        <iframe v-else width="100%" height="315"  :src="url" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h3 class="card-footer">from NASA.gov, {{date}}</h3>
     </div>
 </template>
 
 <script lang="ts">
-    /**
-     * Use of getter and setter to computed value to simplify two way conversion of temperature.
-     */
+
     import Vue from 'vue';
     import axios from 'axios';
 
@@ -25,8 +24,7 @@
             },
             methods: {
                 fetchAPOD : function () {
-                 //   let apiKey = 'DEMO_KEY'; //replace with real apikey. demo key is for limited use only
-                    let apiKey = '7mUh8iioKZFlbIvludFrzdeUauQeReg35rNZUqyE' //nasa.gov api key for dutch@adachis.info
+                    let apiKey = 'DEMO_KEY'; //replace with real apikey. demo key is for limited use only
                     let url = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey;
                     axios.get(url)
                         .then((res) => {
@@ -34,8 +32,13 @@
                            this.title=res.data.title;
                             this.date=res.data.date;
                         })
-                },
+                }
 
+            },
+            computed: {
+                urlisImage: function():boolean {
+                    return (this.url.includes('.jpg'))//APOD api sometimes return youtube Video. Need to address this
+                }
             },
 
             created: function () {
