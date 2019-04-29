c77b7ddc0959e3399492f83a7637f8ebeeca0662
Dutch Adachi <dutch@adachis.info>
2019-04-29 01:30:11 -0400

    Fixed typo

--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="container">
 
-            <h2 class="card-header">Astronomy Picture of the Day. {{date}}</h2>
-            <img v-if="urlIsImage" :src="url" class="img-fluid" :title="title">
-        <iframe v-else width="100%" height="315"  :src="url" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <h3 class="card-footer">from NASA.gov, {{date}}</h3>
+        <h2 class="card-header">Astronomy Picture of the Day. {{date}}</h2>
+        <img v-if="urlisImage" :src="url" class="img-fluid" :title="title">
+        <iframe v-else width="100%" height="315" :src="url" frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+        <h3 class="card-footer">from NASA.gov, {{date}}</h3>
     </div>
 </template>
 
@@ -17,26 +19,26 @@
         {
             data() {
                 return {
-                    url:"",
-                    title:"",
-                    date:""
+                    url: "",
+                    title: "",
+                    date: ""
                 }
             },
             methods: {
-                fetchAPOD : function () {
+                fetchAPOD: function () {
                     let apiKey = 'DEMO_KEY'; //replace with real apikey. demo key is for limited use only
                     let url = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey;
                     axios.get(url)
                         .then((res) => {
-                           this.url=res.data.url;
-                           this.title=res.data.title;
-                            this.date=res.data.date;
+                            this.url = res.data.url;
+                            this.title = res.data.title;
+                            this.date = res.data.date;
                         })
                 }
 
             },
             computed: {
-                urlisImage: function():boolean {
+                urlisImage: function (): boolean {
                     return (this.url.includes('.jpg'))//APOD api sometimes return youtube Video. Need to address this
                 }
             },
@@ -50,5 +52,7 @@
 </script>
 
 <style scoped>
-    [v-cloak] {display:none}
+    [v-cloak] {
+        display: none
+    }
 </style>
